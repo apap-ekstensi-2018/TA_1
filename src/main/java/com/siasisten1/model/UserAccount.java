236a67654d36a3d273a975c0ea28f37b22b6e12e
fpamungkas <muhammad.fajar72@ui.ac.id>
2018-04-17 21:25:55 +0700

    Andesta

--- src/main/java/com/siasisten1/model/UserAccount.java
+++ src/main/java/com/siasisten1/model/UserAccount.java
@@ -1,6 +1,12 @@
 package com.siasisten1.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class UserAccount {
-	
-	
+	private String username,password,role;
 }
