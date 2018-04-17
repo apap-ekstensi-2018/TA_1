3566f4d85b92b3589066a073117f0d37208b3216
fpamungkas <muhammad.fajar72@ui.ac.id>
2018-04-17 20:37:40 +0700

    Skeleton

---
+++ src/main/java/com/siasisten1/SiasistenApplication.java
@@ -0,0 +1,12 @@
+package com.siasisten1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiasistenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiasistenApplication.class, args);
+	}
+}
