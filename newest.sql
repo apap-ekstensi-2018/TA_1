456a82622c3a758ba41d293af47a1349e59566f6
fpamungkas <muhammad.fajar72@ui.ac.id>
2018-05-13 04:06:41 +0700

    sql

---
+++ newest.sql
@@ -0,0 +1,162 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.5.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: May 12, 2018 at 11:02 PM
+-- Server version: 5.6.35
+-- PHP Version: 7.1.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+--
+-- Database: `si_asisten`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `lowongan`
+--
+
+CREATE TABLE `lowongan` (
+  `id` int(11) NOT NULL,
+  `id_matkul` int(11) NOT NULL,
+  `is_open` tinyint(1) NOT NULL,
+  `jumlah_lowongan` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `lowongan`
+--
+
+INSERT INTO `lowongan` (`id`, `id_matkul`, `is_open`, `jumlah_lowongan`) VALUES
+(1, 1, 1, 2),
+(2, 18, 1, 3),
+(3, 2, 0, 9),
+(5, 1, 0, 5);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pengajuan`
+--
+
+CREATE TABLE `pengajuan` (
+  `id` int(11) NOT NULL,
+  `id_lowongan` int(11) NOT NULL,
+  `username_mahasiswa` varchar(250) NOT NULL,
+  `is_accepted` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `pengajuan`
+--
+
+INSERT INTO `pengajuan` (`id`, `id_lowongan`, `username_mahasiswa`, `is_accepted`) VALUES
+(41, 1, '1290578809', 2),
+(43, 2, '1506721775', 1),
+(44, 1, '1506721775', 2);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `username` varchar(45) NOT NULL,
+  `password` varchar(250) NOT NULL,
+  `enabled` tinyint(4) NOT NULL DEFAULT '1'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`username`, `password`, `enabled`) VALUES
+('1290578809', '$2a$10$kPiwZnKcwg4CI5S.qwiBaObe5qsq/IkzglCShn2aDdyqWcRPnZmpW', 1),
+('1506721775', '$2a$10$kPiwZnKcwg4CI5S.qwiBaObe5qsq/IkzglCShn2aDdyqWcRPnZmpW', 1),
+('1506721781', '$2a$10$kPiwZnKcwg4CI5S.qwiBaObe5qsq/IkzglCShn2aDdyqWcRPnZmpW', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_roles`
+--
+
+CREATE TABLE `user_roles` (
+  `user_role_id` int(11) NOT NULL,
+  `username` varchar(45) NOT NULL,
+  `role` varchar(45) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `user_roles`
+--
+
+INSERT INTO `user_roles` (`user_role_id`, `username`, `role`) VALUES
+(5, '1290578809', 'ROLE_ADMIN'),
+(6, '1506721775', 'ROLE_USER'),
+(7, '1506721781', 'ROLE_USER');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `lowongan`
+--
+ALTER TABLE `lowongan`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `pengajuan`
+--
+ALTER TABLE `pengajuan`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`username`);
+
+--
+-- Indexes for table `user_roles`
+--
+ALTER TABLE `user_roles`
+  ADD PRIMARY KEY (`user_role_id`),
+  ADD UNIQUE KEY `uni_username_role` (`role`,`username`),
+  ADD KEY `fk_username_idx` (`username`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `lowongan`
+--
+ALTER TABLE `lowongan`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+--
+-- AUTO_INCREMENT for table `pengajuan`
+--
+ALTER TABLE `pengajuan`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=45;
+--
+-- AUTO_INCREMENT for table `user_roles`
+--
+ALTER TABLE `user_roles`
+  MODIFY `user_role_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `user_roles`
+--
+ALTER TABLE `user_roles`
+  ADD CONSTRAINT `fk_username` FOREIGN KEY (`username`) REFERENCES `users` (`username`);
+
